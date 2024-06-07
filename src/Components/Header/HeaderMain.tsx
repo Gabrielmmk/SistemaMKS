29d5003c618c09919e5ffc647e98be1f06a2f25b
Gabriel Medeiros <gabrielmmk087@gmail.com>
2024-06-07 02:13:25 -0300

    Header finalizado

---
+++ src/Components/Header/HeaderMain.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import './HeaderMain.scss';
+
+const Header = () => {
+    return (
+        <>
+            <nav className="navbar header">
+                <div className="container-fluid" id="name">
+                    <a className="navbar-brand" id="MKS">MKS<span>Sistemas</span></a>
+                    <form className="d-flex">
+                        <button 
+                            className="btn bg-white botao" 
+                            type="button"
+                            data-bs-toggle="offcanvas" 
+                            data-bs-target="#offcanvasRight" 
+                            aria-controls="offcanvasRight"
+                        >
+                            <i className="fas fa-shopping-cart"></i> 0
+                        </button>
+                    </form>
+                </div>
+            </nav>
+
+            <div className="offcanvas offcanvas-end" tabIndex={-1} id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+                <div className="offcanvas-header">
+                    <h5 className="offcanvas-title" id="offcanvasRightLabel">Carrinho de compras</h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div className="offcanvas-body">
+                    Conteúdo do offcanvas
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Header;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import Header from './Components/Header/HeaderMain';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
   </React.StrictMode>
 );
 
